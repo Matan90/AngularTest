b41274b8d7c4a3401cf172f74f3bd38ae598839e
Matan90 <37303256+Matan90@users.noreply.github.com>
2022-01-04 15:16:18 +0200

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, interval, Subscription } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { not } from '@angular/compiler/src/output/output_ast';
+import { Time } from '@angular/common';
 
 
 @Component({
@@ -16,11 +17,12 @@ import { not } from '@angular/compiler/src/output/output_ast';
 export class AppComponent implements OnInit {
   title = 'AngularTestSpaceStation';
   callLogInterval!: Subscription
-  constructor(private service: AppService, public dialog: MatDialog ) { }
+  constructor(private service: AppService, public dialog: MatDialog) { }
 
   data: any;
+  time = new Date();
   logInterval = interval(2000);
-  
+
   ngOnInit() {
 
     this.service.getLocations().subscribe(res => {
@@ -28,29 +30,38 @@ export class AppComponent implements OnInit {
     });
 
     this.callLogInterval = this.logInterval.subscribe(() => {
+      this.service.getLocations().subscribe(res => {
+        this.data = res;
+      });
       this.print();
     })
   }
 
-  print(notes: string = "") {
-    var time = new Date();
+  print(notes: string = "", dialogLocation?: any, dialogTime?: Date) {
+    this.data = dialogLocation ? dialogLocation : this.data;
+    this.time = dialogTime ? dialogTime : new Date();
 
     const div = document.getElementById("consoleDiv");
     const p = document.createElement("p");
     if (notes != "" && notes != null) {
       notes = `. Notes: ${notes}`
     }
-    p.textContent = `[${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}]  Longitude :${this.data.iss_position.longitude},  Longitude :${this.data.iss_position.latitude}${notes}`;
+    else{
+      notes = "";
+    }
+    p.textContent = `[${this.time.getHours()}:${this.time.getMinutes()}:${this.time.getSeconds()}]  Longitude :${this.data.iss_position.longitude},  Latitude :${this.data.iss_position.latitude}${notes}`;
     div?.appendChild(p);
 
   }
 
   openDialog() {
     // this.callLogInterval.unsubscribe();
+    let dialogLocation = this.data;
+    let dialogTime = this.time;
     const dialogRef = this.dialog.open(NotesDialogComponent);
     dialogRef.afterClosed().subscribe((res) => {
       if (res.event = "Save") {
-        this.print(res.notes);
+        this.print(res.notes, dialogLocation, dialogTime);
         // this.callLogInterval = this.logInterval.subscribe(() => {
         //   this.print();
         // });
